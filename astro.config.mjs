673030077f2a464384b385ed7d6198c12f2c4fc9
丝丝大魔王 <sisi@blog.com>
2026-05-29 15:17:53 +0800

    update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://sisidashemowang.github.io',
+  site: 'https://Yuk-Long.github.io',
   base: '/blog',
   markdown: {
     shikiConfig: {
