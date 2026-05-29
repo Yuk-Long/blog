806ff134bb6adda3daf042308a6d40094590a46c
丝丝大魔王 <sisi@blog.com>
2026-05-29 17:06:59 +0800

    fix: restore base path, fix petal animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Yuk-Long.github.io',
+  base: '/blog/',
   markdown: {
     shikiConfig: {
       theme: 'github-light',
