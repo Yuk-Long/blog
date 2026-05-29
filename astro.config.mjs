68860c138518af3e3718c381de73001e42e763f2
丝丝大魔王 <sisi@blog.com>
2026-05-29 15:56:19 +0800

    fix: remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Yuk-Long.github.io',
-  base: '/blog/',
-  trailingSlash: 'always',
   markdown: {
     shikiConfig: {
       theme: 'github-light',
