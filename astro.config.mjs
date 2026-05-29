d9a4e60c3a0119ad00c3eaf6016dcaa96d53f734
丝丝大魔王 <sisi@blog.com>
2026-05-29 15:09:54 +0800

    add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://sisidashemowang.github.io',
+  base: '/blog',
   markdown: {
     shikiConfig: {
       theme: 'github-light',
