9c0a30aff52acb630a4ddb828c912a17fa0b4366
丝丝大魔王 <sisi@blog.com>
2026-05-29 15:54:04 +0800

    fix: add trailing slash config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Yuk-Long.github.io',
-  base: '/blog',
+  base: '/blog/',
+  trailingSlash: 'always',
   markdown: {
     shikiConfig: {
       theme: 'github-light',
