bb24f45c32a439ff667d86c9d9fd6942723b7ac6
丝丝大魔王 <sisi@blog.com>
2026-05-29 16:03:25 +0800

    fix: hardcode /blog/ prefix in all links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Yuk-Long.github.io',
-  base: '/blog/',
   markdown: {
     shikiConfig: {
       theme: 'github-light',
