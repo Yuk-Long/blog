11daf10499e295c5539a1d7e57848eb0f34000e0
丝丝大魔王 <sisi@blog.com>
2026-05-30 15:53:23 +0800

    fix: rename _astro to assets, remove base tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://Yuk-Long.github.io',
-  base: '/blog/',
+  site: "https://Yuk-Long.github.io",
+  base: "/blog/",
+  vite: {
+    build: {
+      assetsDir: "assets",
+    },
+  },
   markdown: {
     shikiConfig: {
-      theme: 'github-light',
+      theme: "github-light",
     },
   },
 });
